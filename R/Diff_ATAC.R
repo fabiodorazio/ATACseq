8e7014bcaa81c1ecd6373667e950a6de2a6c13fe
Fabio M. D'Orazio <34645834+fabiodorazio@users.noreply.github.com>
2024-05-28 10:40:00 +0100

    updated .convert_to_grange function

--- R/Diff_ATAC.R
+++ R/Diff_ATAC.R
@@ -2,19 +2,35 @@
 #atacUp <- read.csv('../All_ATAC_Upreg_PGC_Prim5.txt', sep = '\t')
 #atacDown <- read.csv('../All_ATAC_Upreg_Soma_Prim5.txt', sep = '\t')
 
+#' @include dk
+#' @author Fabio M D'Orazio
+#' @description Differential openess analysis for ATAC peak
+
+#####################################################
+### Perform differential analysis between somatic and PGCs using Chipseeker functions ####
+
+library(dplyr)
+library(GenomicRanges)
+library(ChIPseeker)
+library(AnnotationDbi)
+
 ## create chr and range column from merged rownames to generate a Grange
-convert.to.grange <- function(x){
-  x$chr <- as.character(lapply(strsplit(rownames(x), "\\:"), function(u) u[[1]][1]))
-  x$range <- as.character(lapply(strsplit(rownames(x), "\\:"), function(u) u[[2]][1]))
-  x$start <- as.character(lapply(strsplit(x$range, "\\-"), function(u) u[[1]][1]))
-  x$end <- as.character(lapply(strsplit(x$range, "\\-"), function(u) u[[2]][1]))
+.convert_to_grange <- function(x){
+  x$chr <- as.character(lapply(strsplit(rownames(x), "\\:"), 
+                               function(u) u[[1]][1]))
+  x$range <- as.character(lapply(strsplit(rownames(x), "\\:"), 
+                                 function(u) u[[2]][1]))
+  x$start <- as.character(lapply(strsplit(x$range, "\\-"), 
+                                 function(u) u[[1]][1]))
+  x$end <- as.character(lapply(strsplit(x$range, "\\-"), 
+                               function(u) u[[2]][1]))
   x$range <- NULL
   return(x)
 }
-atacUpRange <- convert.to.grange(atacUp)
-atacDownRange <- convert.to.grange(atacDown)
+atacUpRange <- .convert_to_grange(atacUp)
+atacDownRange <- .convert_to_grange(atacDown)
 
-library(AnnotationDbi)
+
 txdb <- loadDb('../annotation/txdb_DanRer7.sqlite')
 ensembl.ids.atac <- function(x){
   grange.obj <- toGRanges(x)
